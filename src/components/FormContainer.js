f47ad70164793a738c78f20f01bcc73aa6f78f11
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-07-29 10:59:43 +0530

    form container added

---
+++ src/components/FormContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/FormContainer.css';
+
+function FormContainer() {
+  return (
+    <div className='form-container'>FormContainer</div>
+  )
+}
+
+export default FormContainer
