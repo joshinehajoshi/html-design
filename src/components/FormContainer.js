d8452ee55f37601176a432a12268bd16a9acd085
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-07-29 12:26:08 +0530

    form added

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -3,7 +3,25 @@ import '../style/FormContainer.css';
 
 function FormContainer() {
   return (
-    <div className='form-container'>FormContainer</div>
+    <div className='form-container'>
+        <div className='form-wrapper'>
+                <h1>Let us know how we can improve freeCodeCamp</h1>
+                <div className='form-fields'>
+                    <div className='label'><label>First Name</label></div>
+                    <div className='input-field'><input></input></div>
+                </div>
+                <div className='form-fields'>
+                    <div className='label'><label>Last Name</label></div>
+                    <div className='input-field'><input></input></div>
+                </div>
+                <div className='form-fields'>
+                    <div className='label'><label>Email</label></div>
+                    <div className='input-field'><input></input></div>
+                </div>
+               
+        </div>
+
+    </div>
   )
 }
 
